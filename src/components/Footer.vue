952541020b84e2c0311a67a90aa693deffe8a6d1
wrj <1163941040@qq.com>
2022-05-15 20:24:37 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,43 @@
 <template>
- <footer class="content">
-     <div>网站备案号：浙ICP备2021039141号-1</div>
- </footer>
-
+  <footer class="content">
+    <div>网站备案号：浙ICP备2021039141号-1</div>
+    111
+  </footer>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, } from 'vue';
+import { defineComponent, reactive } from "vue";
 export default defineComponent({
-    name: 'Footer',
-    setup() {
-        const iconAttr = reactive({
-            size: 16,
-        });
+  name: "Footer",
+  setup() {
+    const iconAttr = reactive({
+      size: 16,
+    });
 
-        return {
-            iconAttr
-        }
-    },
-    components: {
-    },
-}); 
+    return {
+      iconAttr,
+    };
+  },
+  components: {},
+});
 </script>
 
 <style lang="scss" scoped>
-.content{
-    background-color: $header-color;
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    min-height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 80px;
-    
-    >div{
-        color: #fff;
-        font-size: 14px;
-    }
+.content {
+  background-color: $header-color;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  min-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 80px;
+
+  > div {
+    color: #fff;
+    font-size: 14px;
+  }
 }
 </style>
